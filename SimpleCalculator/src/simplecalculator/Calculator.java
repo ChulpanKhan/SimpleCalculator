cb73c60c2de2cc572a6f4e3fbca58dd8fb1806e4
Ilovemephi <egorkin.2004@mail.ru>
2025-02-17 16:48:56 +0300

    Create Calculator.java

---
+++ SimpleCalculator/src/simplecalculator/Calculator.java
@@ -0,0 +1,16 @@
+
+package simplecalculator;
+
+
+public class Calculator {
+    
+    public double multiply(double a, double b){
+        return a * b;
+    }
+    
+    public double divide(double a, double b){
+        return a/b;
+    }
+    
+    
+}
