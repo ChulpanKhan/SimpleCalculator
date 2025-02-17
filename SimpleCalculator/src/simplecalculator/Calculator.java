0413813846664e13e05f82a2482d99e933098a83
ChulpanKhan <chulpan.khannanova@gmail.com>
2025-02-17 16:49:11 +0300

    Create Calculator.java

---
+++ SimpleCalculator/src/simplecalculator/Calculator.java
@@ -0,0 +1,14 @@
+package simplecalculator;
+
+public class Calculator {
+    
+    public double add(double a, double b) {
+        return a+b;
+    }
+    
+    public double subtract(double a, double b) {
+        return a-b;
+    }
+    
+    
+}
